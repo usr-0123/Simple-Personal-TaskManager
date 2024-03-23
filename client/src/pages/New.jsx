77b97385125c44e53dfa875fcfa13464b50b6c00
usr-0123 <fixedzone365@gmail.com>
2024-03-23 12:17:20 +0300

    Created more functionalities

--- client/src/pages/New.jsx
+++ client/src/pages/New.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate hook
+import { v4 as uuidv4 } from 'uuid'; // Import UUID generator
 
-import './New.scss'
+import './New.scss';
 
 const New = () => {
+  const navigate = useNavigate(); // Initialize useNavigate hook
+
   // State variables for form inputs
   const [taskName, setTaskName] = useState('');
   const [beginDate, setBeginDate] = useState('');
@@ -12,50 +16,81 @@ const New = () => {
   // Function to handle form submission
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Creating a new task object with form input values
+
+    // Generating a unique task ID
+    const taskID = uuidv4();
+
+    // Creating a new task object with form input values and task ID
     const newTask = {
+      
+      id: taskID, // Include taskID in the new task object
       taskName,
       beginDate,
       endDate,
       comment,
     };
+
+    // Check if the task with the same ID already exists
+    if (!isTaskIdUnique(taskID)) {
+      alert('Task with the same ID already exists. Please try again.');
+      return;
+    }
+
     // Saving the new task to local storage
     saveTaskToLocalStorage(newTask);
+
     // Resetting form inputs
     setTaskName('');
     setBeginDate('');
     setEndDate('');
     setComment('');
+
+    // Redirect to the Tasks component
+    navigate('/tasks');
+  };
+
+  // Function to check if the task ID is unique
+  const isTaskIdUnique = (id) => {
+    const existingTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    return !existingTasks.some(task => task.id === id);
   };
 
   // Function to save task to local storage
   const saveTaskToLocalStorage = (task) => {
+
     // Retrieve existing tasks from local storage or initialize as an empty array
     const existingTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
     // Add the new task to the array
     existingTasks.push(task);
+
     // Save the updated tasks array back to local storage
     localStorage.setItem('tasks', JSON.stringify(existingTasks));
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <input type="text" value={taskName} onChange={(e) => setTaskName(e.target.value)} placeholder='Enter task Name'/>
-      </div>
-      <div>
-        <label>Begin Date</label>
-        <input type="date" value={beginDate} onChange={(e) => setBeginDate(e.target.value)} />
-      </div>
-      <div>
-        <label>End Date</label>
-        <input type="date" value={endDate} onChange={(e) => setEndDate(e.target.value)}/>
-      </div>
-      <div>
-        <textarea value={comment} onChange={(e) => setComment(e.target.value)} placeholder='Enter your Comments'/>
-      </div>
-      <button type="submit">Add Task</button>
-    </form>
+    <>
+      <h1>Add a new task</h1>
+      <form onSubmit={handleSubmit} className='newTaskForm'>
+        <div className='newTaskFormName'>
+          <input type="text" value={taskName} onChange={(e) => setTaskName(e.target.value)} placeholder='Enter task Name'/>
+        </div>
+        <div className='newTaskFormDates'>
+          <div className='newTaskFormDatesContainer'>
+            <label>Begin Date</label>
+            <input type="date" value={beginDate} onChange={(e) => setBeginDate(e.target.value)} />
+          </div>
+          <div className='newTaskFormDatesContainer'>
+            <label>End Date</label>
+            <input type="date" value={endDate} onChange={(e) => setEndDate(e.target.value)}/>
+          </div>
+        </div>
+        <div className='newTaskFormComments'>
+          <input className='newTaskFormCommentsInput' value={comment} onChange={(e) => setComment(e.target.value)} placeholder='Enter your Comments'/>
+        </div>
+        <button className='button' type="submit">Add Task</button>
+      </form>
+    </>
   );
 };
 
--- client/src/pages/Tasks.jsx
+++ client/src/pages/Tasks.jsx
@@ -1,26 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import './Tasks.scss';
 
-import { IoIosArrowForward } from "react-icons/io";
-import { IoClose } from "react-icons/io5";
-
 const Tasks = () => {
-  // State variable to store tasks
   const [tasks, setTasks] = useState([]);
-  const [selectedTask, setSelectedTask] = useState(null)
+  const [selectedTask, setSelectedTask] = useState(null);
+  const [editMode, setEditMode] = useState(null);
+  const [editedDetails, setEditedDetails] = useState({});
 
-  // Function to fetch tasks from local storage
   const fetchTasksFromLocalStorage = () => {
     const storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
-    setTasks(storedTasks);
+    const tasksWithId = storedTasks.map(task => ({ ...task, id: task.id }));
+    setTasks(tasksWithId);
   };
 
-  // useEffect hook to fetch tasks when the component mounts
   useEffect(() => {
     fetchTasksFromLocalStorage();
   }, []);
 
-  // Function to handle marking a task as done
   const handleTaskDone = (taskId) => {
     const updatedTasks = tasks.map(task => {
       if (task.id === taskId) {
@@ -32,45 +28,123 @@ const Tasks = () => {
     localStorage.setItem('tasks', JSON.stringify(updatedTasks));
   };
 
-  // Function to toggle display of full content for a task
+  const handleUndoTaskDone = (taskId) => {
+    const updatedTasks = tasks.map(task => {
+      if (task.id === taskId) {
+        return { ...task, done: false };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+  };
+
   const toggleTaskContent = (taskId) => {
     const updatedTasks = tasks.map(task => {
       if (task.id === taskId) {
         return { ...task, showFullContent: !task.showFullContent };
       } else {
-        return { ...task, showFullContent: false }; // Close other tasks
+        return { ...task, showFullContent: false };
       }
     });
     setTasks(updatedTasks);
+    setSelectedTask(taskId);
   };
 
-//   const selectedTask = ()
+  const handleEditTaskDetails = (taskId, field, newValue) => {
+    setEditedDetails(prevState => ({
+      ...prevState,
+      [field]: newValue
+    }));
+  };
 
-console.log();
+  const handleSaveChanges = () => {
+    const updatedTasks = tasks.map(task => {
+      if (task.id === editMode) {
+        return { ...task, ...editedDetails };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+    setEditMode(null);
+  };
+
+  const handleSaveTaskName = () => {
+    const updatedTasks = tasks.map(task => {
+      if (task.id === selectedTask) {
+        return { ...task, taskName: editedDetails.taskName || task.taskName };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+    setSelectedTask(null);
+  };
+
+  const handleDeleteTask = (taskId) => {
+    const updatedTasks = tasks.filter(task => task.id !== taskId);
+    setTasks(updatedTasks);
+    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+  };
 
   return (
-    <div>
-      <h2>Task List</h2>
-      <ul>
+    <div className='tasks'>
+      <h1>Task List</h1>
+      <div className='tasksList'>
         {tasks.map(task => (
-          <li key={task.id}>
+          <div className='tasksListDiv' key={task.id}>
             <label>
               <input type="radio" checked={task.done} onChange={() => handleTaskDone(task.id)} />
-              {task.taskName}
+              <button onClick={() => toggleTaskContent(task.id)}>Show all details</button>
+              <p onClick={() => setSelectedTask(task.id)}>{task.taskName}</p>
+              <p>Status: {task.done ? "Completed" : "Not Completed"}</p>
             </label>
-            {task.showFullContent && (
+            <button onClick={() => setEditMode(task.id)}>Edit</button>
+            <button onClick={() => handleUndoTaskDone(task.id)}>Undo</button>
+            <button onClick={() => handleDeleteTask(task.id)}>Delete</button>
+            {editMode === task.id && (
               <div>
-                <p>Begin Date: {task.beginDate}</p>
-                <p>End Date: {task.endDate}</p>
-                <p>Comment: {task.comment}</p>
+                <input
+                  type="text"
+                  value={editedDetails.beginDate || task.beginDate}
+                  onChange={(e) => handleEditTaskDetails(task.id, 'beginDate', e.target.value)}
+                />
+                <input
+                  type="text"
+                  value={editedDetails.endDate || task.endDate}
+                  onChange={(e) => handleEditTaskDetails(task.id, 'endDate', e.target.value)}
+                />
+                <input
+                  type="text"
+                  value={editedDetails.comment || task.comment}
+                  onChange={(e) => handleEditTaskDetails(task.id, 'comment', e.target.value)}
+                />
+                <button onClick={handleSaveChanges}>Save Changes</button>
               </div>
             )}
-            <button onClick={() => toggleTaskContent(task.id)}>
-              {task.showFullContent ? <IoClose /> : <IoIosArrowForward /> }
-            </button>
-          </li>
+            {task.showFullContent && (
+              <div className='tasksListDivData'>
+                <input
+                  type="text"
+                  value={selectedTask === task.id ? editedDetails.taskName || task.taskName : task.taskName}
+                  onChange={(e) => handleEditTaskDetails(task.id, 'taskName', e.target.value)}
+                  onBlur={handleSaveTaskName}
+                  autoFocus
+                />
+
+                <p>From: {task.beginDate}</p>
+                <p>To: {task.endDate}</p>
+                <p style={{
+                  backgroundColor:"#d1d1d1",
+                  padding:"10px",
+                  borderRadius:"13px"
+                }}>{task.comment}</p>
+              </div>
+            )}
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
